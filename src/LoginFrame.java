6edb994410c0653d96800f6c514b9d1302a050b3
shurikendan <62018439+shurikendan@users.noreply.github.com>
2020-03-19 22:08:51 +0000

    Start

---
+++ src/LoginFrame.java
@@ -0,0 +1,15 @@
+//Package imports
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+public class LoginFrame extends JFrame implements ActionListener {
+    LoginFrame() {
+
+    }
+    @Override
+    public void actionPerformed(ActionEvent actionEvent) {
+    }
+}
