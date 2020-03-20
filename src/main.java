b6a0f5e8a7e078dedc01b8e2df89802343b3c800
shurikendan <62018439+shurikendan@users.noreply.github.com>
2020-03-20 10:55:07 +0000

    Password Start

--- src/main.java
+++ src/main.java
@@ -1,5 +1,8 @@
-public class main {
-    public static void main(String args[]) {
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
 
+public class main {
+    public static void main(String args[]) throws InvalidKeySpecException, NoSuchAlgorithmException {
+        Ada.main("password");
     }
 }
